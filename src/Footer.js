36f88cdff44f1b24b539a867366d3833b92c3f90
saurabhbera11 <saurab.bera@gmail.com>
2021-08-22 12:06:02 +0530

    final commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaLinkedin } from "react-icons/fa";
+import './footer.css'
+function Footer() {
+    return (
+        <div className="footer">
+            <h3>Designed and Created by Sourabh Bera</h3>
+            <a href="https://www.linkedin.com/in/saurabh-bera-610ba0156/"><FaLinkedin size="1.5em" /></a>
+        </div>
+    )
+}
+export default Footer
